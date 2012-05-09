8e73d47f5b1cc0eefcd8b489baf47a941878d98b
Alberto Leal <albertonb@gmail.com>
2012-05-09 00:04:34 -0300

    Refactoring done

--- js/descubra_o_assassino.js
+++ js/descubra_o_assassino.js
@@ -1,62 +1,57 @@
 function Crime () {
-  var assassino = local = arma = null;
+  var assassino = null;
+  var local     = null;
+  var arma      = null;
+  var suspeitos = new Array();
+  var locais    = new Array();
+  var armas     = new Array();
+
+  suspeitos[0] = 'Charles B. Abbage';
+  suspeitos[1] = 'Donald Duck Knuth';
+  suspeitos[2] = 'Ada L. Ovelace';
+  suspeitos[3] = 'Alan T. Uring';
+  suspeitos[4] = 'Ivar J. Acobson';
+  suspeitos[5] = 'Ras Mus Ler Dorf';
   
-  this.compor_elementos = function () {
+  locais[0] = 'Redmond';
+  locais[1] = 'Palo Alto';
+  locais[2] = 'San Francisco';
+  locais[3] = 'Tokio';
+  locais[4] = 'Restaurante no Fim do Universo';
+  locais[5] = 'São Paulo';
+  locais[6] = 'Cupertino';
+  locais[7] = 'Helsinki';
+  locais[8] = 'Maida Vale';
+  locais[9] = 'Toronto';
+  
+  armas[0] = 'Peixeira';
+  armas[1] = 'DynaTAC 8000X';
+  armas[2] = 'Trezoitão';
+  armas[3] = 'Trebuchet';
+  armas[4] = 'Maça';
+  armas[5] = 'Gládio';
     
+  this.compor_cena = function () {
+    assassino = random_pistas(suspeitos);
+    local     = random_pistas(locais);
+    arma      = random_pistas(armas);
   }
 
   this.get_assassino = function () {
-    var suspeitos = new Array();
-    
-    suspeitos[0] = 'Charles B. Abbage';
-    suspeitos[1] = 'Donald Duck Knuth';
-    suspeitos[2] = 'Ada L. Ovelace';
-    suspeitos[3] = 'Alan T. Uring';
-    suspeitos[4] = 'Ivar J. Acobson';
-    suspeitos[5] = 'Ras Mus Ler Dorf';
-    
-    var max_suspeitos = suspeitos.length;
-    
-    suspeito = Math.floor(Math.random(suspeitos) * max_suspeitos);
-    
-    return suspeito;
+    return suspeitos[assassino];
   }
 
   this.get_local = function () {
-    var locais = new Array();
-    
-    locais[0] = 'Redmond';
-    locais[1] = 'Palo Alto';
-    locais[2] = 'San Francisco';
-    locais[3] = 'Tokio';
-    locais[4] = 'Restaurante no Fim do Universo';
-    locais[5] = 'São Paulo';
-    locais[6] = 'Cupertino';
-    locais[7] = 'Helsinki';
-    locais[8] = 'Maida Vale';
-    locais[9] = 'Toronto';
-    
-    var max_locais = locais.length;
-    
-    local = Math.floor(Math.random(locais) * max_locais);
-  
-    return local;
+    return locais[local];
   }
   
   this.get_arma = function () {
-    var armas = new Array();
-
-    armas[0] = 'Peixeira';
-    armas[1] = 'DynaTAC 8000X';
-    armas[2] = 'Trezoitão';
-    armas[3] = 'Trebuchet';
-    armas[4] = 'Maça';
-    armas[5] = 'Gládio';
-    
-    var max_armas = armas.length;
-    
-    arma = Math.floor(Math.random(armas) * max_armas);
-    
-    return arma;
+    return armas[arma];
   } 
+  
+  function random_pistas (pistas) {
+    var max_pistas = pistas.length;    
+    pista = Math.floor(Math.random(pistas) * max_pistas);    
+    return pista;
+  }
 }
--- tests/spec/descubra_o_assassino_spec.js
+++ tests/spec/descubra_o_assassino_spec.js
@@ -1,8 +1,8 @@
 describe("Crime", function() {
   it("deve nos permitir escolher o assassino, local e arma", function() {
     crime = new Crime;
-    crime.compor_elementos();
-     
+    crime.compor_cena();
+    
     expect(crime.get_assassino()).not.toBeNull();
     expect(crime.get_local()).not.toBeNull();
     expect(crime.get_arma()).not.toBeNull();
